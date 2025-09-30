2baf5f2cd762d5b414090cf477dbc11f52d03bfa
sk1fix <agafonovtoha427@gmail.com>
2025-09-30 14:58:38 +0400

    feat(Bike.cs): add a bike specifications class

---
+++ BikeRentalPoint.Domain/Models/Bike.cs
@@ -0,0 +1,27 @@
+﻿namespace BikeRentalPoint.Domain.Models;
+
+/// <summary>
+/// Information about bike
+/// </summary>
+public class Bike
+{
+    /// <summary>
+    /// Unique identifier of the bike
+    /// </summary>
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    /// <summary>
+    /// Bike serial number
+    /// </summary>
+    public required int SerialNumber { get; set; }
+
+    /// <summary>
+    /// Bike color
+    /// </summary>
+    public required string Color { get; set; }
+
+    /// <summary>
+    /// Model associated with this bike
+    /// </summary>
+    public required Model Model { get; set; }
+}
