81cde6c661ad9cfe63c3d78ca12cd6aef65f2b66
sk1fix <agafonovtoha427@gmail.com>
2025-09-30 15:19:43 +0400

    feat(Renter.cs): add bicycle renter information class

---
+++ BikeRentalPoint.Domain/Models/Renter.cs
@@ -0,0 +1,32 @@
+﻿namespace BikeRentalPoint.Domain.Models;
+
+/// <summary>
+/// Information about the bike renter
+/// </summary>
+public class Renter
+{
+    /// <summary>
+    /// Unique identifier for the renter
+    /// </summary>
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    /// <summary>
+    /// Last name of the renter
+    /// </summary>
+    public required string LastName { get; set; }
+
+    /// <summary>
+    /// First name of the renter
+    /// </summary>
+    public required string Name { get; set; }
+
+    /// <summary>
+    /// Patronymic of the renter
+    /// </summary>
+    public required string MiddleName { get; set; }
+
+    /// <summary>
+    /// Contact phone number 
+    /// </summary>
+    public required string PhoneNumber { get; set; }
+}
