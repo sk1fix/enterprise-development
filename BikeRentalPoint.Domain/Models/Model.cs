00287efd0b1a4cd0de2cf64f9b4539cd9f21bf51
sk1fix <agafonovtoha427@gmail.com>
2025-09-30 14:30:51 +0400

    feat(Model.cs): add information class about bike model

---
+++ BikeRentalPoint.Domain/Models/Model.cs
@@ -0,0 +1,47 @@
+﻿namespace BikeRentalPoint.Domain.Models;
+
+/// <summary>
+/// Information about the bike model
+/// </summary>
+public class Model
+{
+    /// <summary>
+    /// Unique identifier of the model
+    /// </summary>
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    /// <summary>
+    /// Bike wheel size
+    /// </summary>
+    public required float WheelSize { get; set; }
+
+    /// <summary>
+    /// Maximum permissible passenger weight
+    /// </summary>
+    public required int MaxPassengerWeight { get; set; }
+
+    /// <summary>
+    /// Bike weight
+    /// </summary>
+    public required float BikeWeight { get; set; }
+
+    /// <summary>
+    /// Type of bicycle brakes
+    /// </summary>
+    public required string BrakeType { get; set; } 
+
+    /// <summary>
+    /// Model year
+    /// </summary>
+    public required int ModelYear { get; set; }
+
+    /// <summary>
+    /// Price of an hour of rent
+    /// </summary>
+    public required decimal PricePerHour { get; set; }
+
+    /// <summary>
+    /// Type of bicycle model
+    /// </summary>
+    public required BikeType BikeType { get; set; }
+}
