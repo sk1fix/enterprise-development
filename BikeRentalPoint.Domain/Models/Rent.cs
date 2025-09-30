0d021060ce06f71b3499da916f5620886c174b41
sk1fix <agafonovtoha427@gmail.com>
2025-09-30 15:44:49 +0400

    feat(Rent.cs): add a rental information class

---
+++ BikeRentalPoint.Domain/Models/Rent.cs
@@ -0,0 +1,32 @@
+﻿namespace BikeRentalPoint.Domain.Models;
+
+/// <summary>
+/// Bike rental information
+/// </summary>
+public class Rent
+{
+    /// <summary>
+    /// Unique identifier of the rent
+    /// </summary>
+    public Guid Id { get; set; } = Guid.NewGuid();
+
+    /// <summary>
+    /// Date and time when the rental period started
+    /// </summary>
+    public required DateTime StartTime { get; set; }
+
+    /// <summary>
+    /// Duration of the rental period in hours
+    /// </summary>
+    public required decimal Duration { get; set; }
+
+    /// <summary>
+    /// Bike associated with this rent
+    /// </summary>
+    public required Bike Bike { get; set; }
+
+    /// <summary>
+    /// Renter associated with this rent
+    /// </summary>
+    public required Renter Renter { get; set; }
+}
