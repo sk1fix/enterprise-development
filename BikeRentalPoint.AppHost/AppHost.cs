a6335e139d69c94d29e294110918441ac7023264
sk1fix <agafonovtoha427@gmail.com>
2025-12-15 21:50:10 +0400

    feat: add BikeKafkaConsumer.cs with consumer

--- BikeRentalPoint.AppHost/AppHost.cs
+++ BikeRentalPoint.AppHost/AppHost.cs
@@ -3,8 +3,21 @@ var builder = DistributedApplication.CreateBuilder(args);
 var mysql = builder.AddMySql("mysql-bikerentalpoint")
     .AddDatabase("BikeRentalPointDb");
 
-builder.AddProject<Projects.BikeRentalPoint_Api_Host>("api")
+var apiHost = builder.AddProject<Projects.BikeRentalPoint_Api_Host>("api")
     .WithReference(mysql, "DefaultConnection")
     .WaitFor(mysql);
+
+var kafka = builder.AddKafka("bookstore-kafka")
+    .WithKafkaUI();
+
+var kafkaTopic = builder.AddParameter("KafkaTopic");
+builder.AddProject<Projects.BookStore_Generator_Kafka_Host>("bike-rental-point-generator-kafka-host")
+    .WithReference(kafka)
+    .WaitFor(kafka)
+    .WithEnvironment("Kafka:TopicName", kafkaTopic);
+
+apiHost.WithEnvironment("Kafka:TopicName", kafkaTopic)
+    .WithReference(kafka)
+    .WaitFor(kafka);
 
 builder.Build().Run();
---
+++ BikeRentalPoint.Infrastructure.Kafka/BikeKafkaConsumer.cs
@@ -0,0 +1,67 @@
+﻿using BikeRentalPoint.Application.Contracts.Rent;
+using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+
+namespace BikeRentalPoint.Infrastructure.Kafka;
+/// <summary>
+/// Background service for consuming bike rental requests from Kafka topics
+/// </summary>
+/// <param name="consumer">Kafka consumer instance for processing rental messages</param>
+/// <param name="scopeFactory">Factory for creating service scopes for dependency resolution</param>
+/// <param name="configuration">Application configuration for accessing Kafka settings</param>
+/// <param name="logger">Logger for tracking consumer activities and errors</param>
+public class BikeKafkaConsumer(IConsumer<Guid, IList<CreateRentDto>> consumer, IServiceScopeFactory scopeFactory, IConfiguration configuration, ILogger<BikeKafkaConsumer> logger) : BackgroundService
+{
+    private readonly string _topicName = configuration.GetSection("Kafka")["TopicName"] ?? throw new KeyNotFoundException("TopicName section of Kafka is missing");
+
+    /// <summary>
+    /// Main execution method for the background service
+    /// </summary>
+    /// <param name="stoppingToken">Cancellation token to stop the consumer service</param>
+    /// <returns>Task representing the asynchronous operation</returns>
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        stoppingToken.ThrowIfCancellationRequested();
+
+        await Task.Yield();
+        await Consume(stoppingToken);
+    }
+
+    /// <summary>
+    /// Continuously consumes rental contracts from Kafka topic and processes them
+    /// </summary>
+    /// <param name="stoppingToken">Cancellation token to stop consumption</param>
+    /// <returns>Task representing the consumption process</returns>
+    private async Task Consume(CancellationToken stoppingToken)
+    {
+        consumer.Subscribe(_topicName);
+        logger.LogInformation("Consumer successfully subscribed to topic {topic}", _topicName);
+
+        try
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                logger.LogInformation("Consuming from topic {topic} via consumer {consumer}", _topicName, consumer.Name);
+                var consumeResult = consumer.Consume(stoppingToken);
+
+                using var scope = scopeFactory.CreateScope();
+                var rentService = scope.ServiceProvider.GetRequiredService<IRentService>();
+                foreach (var contract in consumeResult.Message.Value)
+                {
+                    await rentService.Create(contract);
+                }
+                logger.LogInformation("Successfully consumed message {key} from topic {topic} via consumer {consumer}", consumeResult.Message.Key, _topicName, consumer.Name);
+                consumer.Commit();
+            }
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "Exception occured during receiving contracts from {topic}", _topicName);
+        }
+    }
+}
+
